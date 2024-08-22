c6d0c4b269b8efc0c4fdebdabe633c81067db256
Nicolas Milhem <nicolasmilhem@gmail.com>
2024-08-22 16:08:30 +0200

    💄 Add custom fonts

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,12 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				title: 'Roboto, sans-serif',
+				text: 'Nunito Variable, sans-serif'
+			},
+		},
 	},
 	plugins: [],
 }
