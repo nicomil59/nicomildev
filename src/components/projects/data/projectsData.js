43c728a506dd89dd2ed8811d6324ff63047d5b28
Nicolas Milhem <nicolasmilhem@gmail.com>
2024-09-13 17:25:06 +0200

    ♻️ PROJECTS > Create projectData and FeaturedProjectCard

---
+++ src/components/projects/data/projectsData.js
@@ -0,0 +1,47 @@
+export const featuredProjects = [
+  {
+    title: "Instagrum",
+    description:
+      "Clone d’une page profil Instagram développé avec React. Mise en œuvre d'une UI responsive qui respecte l'esthétique du réseau social.",
+    image: {
+      src: "instagrum-capture.png",
+      alt: "Capture d'écran du projet Instagrum",
+      width: "700",
+      height: "400"
+    },
+    technos: ["react", "styled components"],
+    githubUrl: "https://github.com/nicomil59/instagrum",
+    demoUrl: "https://instagrum.vercel.app/",
+    pageUrl: "#",
+  },
+  {
+    title: "React TV",
+    description:
+      "Application permettant de rechercher des séries TV dans une base de données externe via une API et de sauvegarder ses titres favoris.",
+    image: {
+      src: "react-tv-capture.png",
+      alt: "Capture d'écran du projet React TV",
+      width: "700",
+      height: "400"
+    },
+    technos: ["react", "sass", "axios"],
+    githubUrl: "https://github.com/nicomil59/react-tv",
+    demoUrl: "https://nicomil59.github.io/react-tv",
+    pageUrl: "#",
+  },
+  {
+    title: "Planning Séries",
+    description:
+      "Calendrier des sorties des séries TV sur les chaînes et plateformes françaises, conçu avec le framework Astro JS.",
+    image: {
+      src: "planning-series-capture.png",
+      alt: "Capture d'écran du projet Planning Séries",
+      width: "700",
+      height: "400"
+    },
+    technos: ["astro", "tailwind css", "day.js"],
+    githubUrl: "https://github.com/nicomil59/planning-astro",
+    demoUrl: "https://planningseries.fr/",
+    pageUrl: "#",
+  }
+];
